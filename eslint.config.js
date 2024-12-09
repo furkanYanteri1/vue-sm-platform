aefdd06f06b54874c7046c1b6e7bb41ec135330c
Furkan Yanteri <furkanyanteri@gmail.com>
2024-12-10 02:07:15 +0300

    format: some linting configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import prettierPlugin from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import' // Added import plugin
 
 export default [
   {
@@ -21,13 +22,17 @@ export default [
   skipFormatting,
   // Add the prettier plugin configuration:
   {
-    plugins: { prettier: prettierPlugin },
+    // Added import plugin to the plugins object
+    plugins: { prettier: prettierPlugin, import: importPlugin },
     rules: {
       // This turns Prettier issues into ESLint errors:
       'prettier/prettier': 'error',
 
       // your other rules like no-multiple-empty-lines:
       'no-multiple-empty-lines': ['error', { max: 1 }],
+
+      // Require a newline after imports:
+      'import/newline-after-import': ['error', { count: 1 }],
     },
   },
 ]
