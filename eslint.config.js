4b8b676d4b01feb5f96319c6c09768d11c3e9436
Furkan Yanteri <furkanyanteri@gmail.com>
2024-12-10 00:33:07 +0300

    format: eslint and prettier is set to show errors as red underlines on save and lets the coder fix the issues

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,32 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
   {
-    name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
   {
-    name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
+  // Add the prettier plugin configuration:
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      // This turns Prettier issues into ESLint errors:
+      'prettier/prettier': 'error',
+
+      // your other rules like no-multiple-empty-lines:
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+    },
+  },
 ]
