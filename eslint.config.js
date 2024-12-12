7f5eda70bd06fa81eecf8ba0e0a6c3a21ad5fd03
Furkan Yanteri <furkanyanteri@gmail.com>
2024-12-12 20:39:17 +0300

    format: some code styling rules applied

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,8 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import prettierPlugin from 'eslint-plugin-prettier'
-import importPlugin from 'eslint-plugin-import' // Added import plugin
-
+import importPlugin from 'eslint-plugin-import'
+// Added import plugin
 export default [
   {
     files: ['**/*.{js,mjs,jsx,vue}'],
@@ -12,7 +12,6 @@ export default [
   {
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
@@ -20,19 +19,26 @@ export default [
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
-  // Add the prettier plugin configuration:
   {
-    // Added import plugin to the plugins object
-    plugins: { prettier: prettierPlugin, import: importPlugin },
+    plugins: {
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
     rules: {
-      // This turns Prettier issues into ESLint errors:
       'prettier/prettier': 'error',
-
-      // your other rules like no-multiple-empty-lines:
-      'no-multiple-empty-lines': ['error', { max: 1 }],
-
-      // Require a newline after imports:
       'import/newline-after-import': ['error', { count: 1 }],
+      'vue/block-spacing': 'error', // Prevent spaces inside braces
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1, // No blank lines allowed within styles
+          maxEOF: 1, // Allow one blank line at the end of file
+          maxBOF: 0, // No blank lines at the start
+        },
+      ],
+      // Turn Prettier issues into ESLint errors
+      // Prevent multiple blank lines in all files
+      // Require a newline after imports
     },
   },
 ]
